6e547f5c28a0d21fafcebf806f741b422ce045c2
Сёмик <@DESKTOP-SACQJDG>
2023-01-23 14:59:35 +0500

    Добавьте файлы проекта.

---
+++ LabFive/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string g = " ";
+        double lastNum = 0;
+        while (g != "q")
+        {
+            g = Console.ReadLine();
+            if (int.TryParse(g, out int t)) 
+            {
+                Console.WriteLine((char)t);
+                lastNum = t; 
+            }
+            
+            else if (double.TryParse(g, out double d))
+            {
+                if (d==lastNum)
+                {
+                    break;
+                }
+                lastNum= d;
+            }
+        }
+    }
+}
